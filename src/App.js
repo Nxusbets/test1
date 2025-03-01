a1c762ece44715f808fddfd626f8bc2deabd407a
Juan Carlos Díaz <169100845+Nxusbets@users.noreply.github.com>
2025-03-01 22:00:38 +0000

    build done

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import Login from './components/Login';
-import Register
-
-from './components/Register';
+import Register from './components/Register';
 function App() {
   return (
     <div className="App">
